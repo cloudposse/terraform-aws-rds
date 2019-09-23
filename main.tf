a5b0a6b47aadfe721dd1286957b53ce2420a1783
Oscar <46930728+osulli@users.noreply.github.com>
2019-09-23 14:35:25 +0100

    Ignore changes to database password

--- main.tf
+++ main.tf
@@ -67,6 +67,10 @@ resource "aws_db_instance" "default" {
   tags                        = module.label.tags
   deletion_protection         = var.deletion_protection
   final_snapshot_identifier   = length(var.final_snapshot_identifier) > 0 ? var.final_snapshot_identifier : module.final_snapshot_label.id
+
+  lifecycle {
+    ignore_changes = ["password"]
+  }    
 }
 
 resource "aws_db_parameter_group" "default" {
