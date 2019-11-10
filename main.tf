35aacbf6e7ef2897b42ee68bfaef1cb949d9133e
Federico Márquez Córdova <fedemzcor@MacBook-Air-de-Federico.local>
2019-11-10 03:15:36 -0600

    se arregland los cidr_blocks

--- main.tf
+++ main.tf
@@ -163,5 +163,5 @@ resource "aws_security_group_rule" "ingress_cidr_blocks" {
   to_port           = var.database_port
   protocol          = "tcp"
   cidr_blocks       = var.allowed_cidr_blocks
-  security_group_id = aws_security_group.default[0].id)
+  security_group_id = aws_security_group.default[0].id
 }
