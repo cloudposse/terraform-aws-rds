533de9706ae43abf0f010b609398177c18abf21c
Fede Córdova <fede.mzcor@gmail.com>
2020-01-26 07:23:35 -0600

    new variable for ca_cert_identifier
    ca_cert_identifier variable was added

--- variables.tf
+++ variables.tf
@@ -50,6 +50,13 @@ variable "database_name" {
   description = "The name of the database to create when the DB instance is created"
 }
 
+variable "ca_cert_identifier" {
+  type        = string
+  description = "The identifier of the CA certificate for the DB instance"
+  default     = "rds-ca-2019"
+    
+}
+
 variable "database_user" {
   type        = string
   default     = ""
