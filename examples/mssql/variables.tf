ab39f6af0299a5ff6afba6e29b74f29614a650ab
Amit Karpe <amitkarpe@gmail.com>
2021-01-08 01:59:54 +0800

    Added Microsoft SQL Server example (#79)
    Co-authored-by: Joe Niland <joe@originalmind.com.au>
    Co-authored-by: actions-bot <58130806+actions-bot@users.noreply.github.com>

---
+++ examples/mssql/variables.tf
@@ -0,0 +1,132 @@
+variable "region" {
+  type        = string
+  description = "AWS region"
+}
+
+variable "availability_zones" {
+  type = list(string)
+}
+
+variable "namespace" {
+  type        = string
+  description = "Namespace (e.g. `eg` or `cp`)"
+}
+
+variable "stage" {
+  type        = string
+  description = "Stage (e.g. `prod`, `dev`, `staging`, `infra`)"
+}
+
+variable "name" {
+  type        = string
+  description = "Name  (e.g. `app` or `cluster`)"
+}
+
+variable "database_name" {
+  type        = string
+  description = "The name of the database to create when the DB instance is created"
+}
+
+variable "database_user" {
+  type        = string
+  description = "Username for the master DB user"
+}
+
+variable "database_password" {
+  type        = string
+  description = "Password for the master DB user"
+}
+
+variable "database_port" {
+  type        = number
+  description = "Database port (_e.g._ `3306` for `MySQL`). Used in the DB Security Group to allow access to the DB instance from the provided `security_group_ids`"
+}
+
+variable "deletion_protection" {
+  type        = bool
+  description = "Set to true to enable deletion protection on the RDS instance"
+}
+
+variable "multi_az" {
+  type        = bool
+  description = "Set to true if multi AZ deployment must be supported"
+}
+
+variable "storage_type" {
+  type        = string
+  description = "One of 'standard' (magnetic), 'gp2' (general purpose SSD), or 'io1' (provisioned IOPS SSD)"
+}
+
+variable "storage_encrypted" {
+  type        = bool
+  description = "(Optional) Specifies whether the DB instance is encrypted. The default is false if not specified"
+}
+
+variable "allocated_storage" {
+  type        = number
+  description = "The allocated storage in GBs"
+}
+
+variable "engine" {
+  type        = string
+  description = "Database engine type"
+  # http://docs.aws.amazon.com/cli/latest/reference/rds/create-db-instance.html
+  # - mysql
+  # - postgres
+  # - oracle-*
+  # - sqlserver-*
+}
+
+variable "engine_version" {
+  type        = string
+  description = "Database engine version, depends on engine type"
+  # http://docs.aws.amazon.com/cli/latest/reference/rds/create-db-instance.html
+}
+
+variable "major_engine_version" {
+  type        = string
+  description = "Database MAJOR engine version, depends on engine type"
+  # https://docs.aws.amazon.com/cli/latest/reference/rds/create-option-group.html
+}
+
+variable "instance_class" {
+  type        = string
+  description = "Class of RDS instance"
+  # https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/Concepts.DBInstanceClass.html
+}
+
+variable "db_parameter_group" {
+  type        = string
+  description = "Parameter group, depends on DB engine used"
+  # "mysql5.6"
+  # "postgres9.5"
+}
+
+variable "publicly_accessible" {
+  type        = bool
+  description = "Determines if database can be publicly available (NOT recommended)"
+}
+
+variable "apply_immediately" {
+  type        = bool
+  description = "Specifies whether any database modifications are applied immediately, or during the next maintenance window"
+}
+
+variable "subnet_ids" {
+  description = "A list of VPC subnet IDs"
+  default     = []
+  type        = list(string)
+}
+
+variable "security_group_ids" {
+  description = "List of VPC security groups to associate"
+  default     = []
+  type        = list(string)
+}
+
+variable "vpc_id" {
+  type        = string
+  default     = ""
+  description = "VPC ID the DB instance will be created in"
+}
+
