82b534f8053457c892967b76615184b947c205d3
nitrocode <nitrocode@users.noreply.github.com>
2021-07-08 20:00:46 -0400

    SSM Params map, default, and merge

--- systems-manager.tf
+++ systems-manager.tf
@@ -5,7 +5,7 @@ variable "kms_alias_name_ssm" {
   description = "KMS alias name for SSM"
 }
 
-variable "ssm_enabled" {
+variable "ssm_parameters_enabled" {
   type        = bool
   default     = false
   description = "If `true` create SSM keys for the database user and password."
@@ -14,7 +14,7 @@ variable "ssm_enabled" {
 variable "ssm_key_format" {
   type        = string
   default     = "/%v/%v/%v"
-  description = "SSM path format. The values will will be used in the following order: `var.ssm_key_prefix`, `var.database_name`, `var.ssm_key_*`"
+  description = "SSM path format. The values will will be used in the following order: `var.ssm_key_prefix`, `module.this.name`, `var.ssm_key_*`"
 }
 
 variable "ssm_key_prefix" {
@@ -23,76 +23,74 @@ variable "ssm_key_prefix" {
   description = "SSM path prefix. Omit the leading forward slash `/`."
 }
 
-variable "ssm_key_user" {
-  type        = string
-  default     = "admin/db_user"
-  description = "The SSM key to save the user. See `var.ssm_path_format`."
+variable "save_parameter_ssm_map_default" {
+  type = any
+  default = {
+    user = {
+      suffix      = "admin/db_user"
+      description = "RDS DB user"
+      type        = "String"
+      overwrite   = true
+    }
+    password = {
+      suffix      = "admin/db_password"
+      description = "RDS DB password"
+      type        = "SecureString"
+      overwrite   = true
+    }
+    hostname = {
+      suffix      = "admin/db_hostname"
+      description = "RDS DB hostname"
+      type        = "String"
+      overwrite   = true
+    }
+    port = {
+      suffix      = "admin/db_port"
+      description = "RDS DB port"
+      type        = "String"
+      overwrite   = true
+    }
+  }
+  description = "The map of ssm keys to the `aws_ssm_parameter` resource. The `description`, `type`, and `overwrite` are passed directly to that resource. The `suffix` is the last value in the `var.ssm_key_format`. If the `type = \"SecureString\"` then the `key_id = var.kms_alias_name_ssm`."
 }
 
-variable "ssm_key_password" {
-  type        = string
-  default     = "admin/db_password"
-  description = "The SSM key to save the password. See `var.ssm_path_format`."
-}
-
-variable "ssm_key_hostname" {
-  type        = string
-  default     = "admin/db_hostname"
-  description = "The SSM key to save the hostname. See `var.ssm_path_format`."
-}
-
-variable "ssm_key_port" {
-  type        = string
-  default     = "admin/db_port"
-  description = "The SSM key to save the port. See `var.ssm_path_format`."
+variable "save_parameter_ssm_map_merge" {
+  type        = any
+  default     = {}
+  description = "This map will be merged with `var.save_parameter_ssm_map_default` so the defaults can be reused while only overwriting a handful of keys."
 }
 
 locals {
-  ssm_enabled = local.enabled && var.ssm_enabled
+  ssm_parameters_enabled = local.enabled && var.ssm_parameters_enabled
+
+  # This will merge the save_parameter_ssm_map defaults with the save_parameter_ssm_map_merge
+  # This is more of a deep merge than a simple merge. A simple merge would require all of the
+  # same keys.
+  save_parameter_ssm_map = {
+    for k, v in var.save_parameter_ssm_map_default :
+    k => merge(v, try(var.save_parameter_ssm_map_merge[k], {}))
+  }
+
+  value_parameter_map = {
+    user     = local.database_user
+    password = local.database_password
+    hostname = module.dns_host_name.hostname
+    port     = var.database_port
+  }
 }
 
-resource "aws_ssm_parameter" "rds_database_user" {
-  count = local.ssm_enabled ? 1 : 0
+resource "aws_ssm_parameter" "rds_database" {
+  for_each = local.ssm_parameters_enabled ? local.save_parameter_ssm_map : {}
 
-  name        = format(var.ssm_key_format, var.ssm_key_prefix, var.database_name, var.ssm_key_user)
-  value       = local.database_user
-  description = "RDS DB user"
-  type        = "String"
-  overwrite   = true
-}
-
-resource "aws_ssm_parameter" "rds_database_password" {
-  count = local.ssm_enabled ? 1 : 0
-
-  name        = format(var.ssm_key_format, var.ssm_key_prefix, var.database_name, var.ssm_key_password)
-  value       = local.database_password
-  description = "RDS DB password"
-  type        = "SecureString"
-  key_id      = var.kms_alias_name_ssm
-  overwrite   = true
-}
-
-resource "aws_ssm_parameter" "rds_database_hostname" {
-  count = local.ssm_enabled ? 1 : 0
-
-  name        = format(var.ssm_key_format, var.ssm_key_prefix, var.database_name, var.ssm_key_hostname)
-  value       = module.dns_host_name.hostname
-  description = "RDS DB hostname"
-  type        = "String"
-  overwrite   = true
-}
-
-resource "aws_ssm_parameter" "rds_database_port" {
-  count = local.ssm_enabled ? 1 : 0
-
-  name        = format(var.ssm_key_format, var.ssm_key_prefix, var.database_name, var.ssm_key_port)
-  value       = var.database_port
-  description = "RDS DB port"
-  type        = "String"
-  overwrite   = true
+  name        = format(var.ssm_key_format, var.ssm_key_prefix, module.this.name, each.value.suffix)
+  value       = local.value_parameter_map[each.key]
+  description = each.value.description
+  type        = each.value.type
+  key_id      = each.value.type == "SecureString" ? var.kms_alias_name_ssm : null
+  overwrite   = each.value.overwrite
 }
 
 output "rds_database_ssm_key_prefix" {
-  value       = local.ssm_enabled ? format(var.ssm_key_format, var.ssm_key_prefix, var.database_name, "") : null
+  value       = local.ssm_parameters_enabled ? format(var.ssm_key_format, var.ssm_key_prefix, module.this.name, "") : null
   description = "SSM prefix"
 }
