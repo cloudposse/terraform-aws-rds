a13e342b4f71e8babc993ef526cfdad44a5796fa
chaitanya Vijay Koya <vijay@BWKC.local>
2022-04-06 21:46:07 +0530

    made changes

--- replica_outputs.tf
+++ replica_outputs.tf
@@ -1,14 +1,14 @@
-output "instance_id" {
+output "replica_instance_id" {
   value       = aws_db_instance.default.*.id
   description = "ID of the instance"
 }
 
-output "instance_address" {
+output "replica_instance_address" {
   value       = aws_db_instance.default.*.address
   description = "Address of the instance"
 }
 
-output "instance_endpoint" {
+output "replica_instance_endpoint" {
   value       = aws_db_instance.default.*.endpoint
   description = "DNS Endpoint of the instance"
 }
