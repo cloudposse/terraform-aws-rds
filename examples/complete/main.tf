573134ca118ebc59f8251bf4f5728adff09bb9bf
nitrocode <7775707+nitrocode@users.noreply.github.com>
2022-06-17 09:37:56 -0500

    Bump examples/complete versions, omit patch version

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 module "vpc" {
   source  = "cloudposse/vpc/aws"
-  version = "0.21.1"
+  version = "1.1.0"
 
   cidr_block = "172.16.0.0/16"
 
@@ -13,12 +13,12 @@ module "vpc" {
 
 module "subnets" {
   source  = "cloudposse/dynamic-subnets/aws"
-  version = "0.38.0"
+  version = "2.0.2"
 
   availability_zones   = var.availability_zones
   vpc_id               = module.vpc.vpc_id
-  igw_id               = module.vpc.igw_id
-  cidr_block           = module.vpc.vpc_cidr_block
+  igw_id               = [module.vpc.igw_id]
+  ipv4_cidr_block      = [module.vpc.vpc_cidr_block]
   nat_gateway_enabled  = false
   nat_instance_enabled = false
 
