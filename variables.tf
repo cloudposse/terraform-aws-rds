a7c03da2838f393471f71f1fdaa3288e1718e079
Jakub Rosa <jakub.rosa@itmagination.com>
2023-06-03 18:27:53 +0200

    fix: chaneg default value for database_manage_master_user_password

--- variables.tf
+++ variables.tf
@@ -48,7 +48,7 @@ variable "database_password" {
 
 variable "database_manage_master_user_password" {
   type        = bool
-  default     = false
+  default     = null
   description = "Set to true to allow RDS to manage the master user password in Secrets Manager. Cannot be set if `database_password` is provided."
 }
 
