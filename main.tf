b382b210c28776551eeb0681e7734544526bef9e
Joe Niland <joe@originalmind.com.au>
2023-11-17 10:35:46 +1100

    Add restore_to_point_in_time variable (#165)

--- main.tf
+++ main.tf
@@ -88,6 +88,18 @@ resource "aws_db_instance" "default" {
   monitoring_interval = var.monitoring_interval
   monitoring_role_arn = var.monitoring_role_arn
 
+  dynamic "restore_to_point_in_time" {
+    for_each = var.snapshot_identifier == null && var.restore_to_point_in_time != null ? [1] : []
+
+    content {
+      restore_time                             = lookup(var.restore_to_point_in_time, "restore_time", null)
+      source_db_instance_identifier            = lookup(var.restore_to_point_in_time, "source_db_instance_identifier", null)
+      source_db_instance_automated_backups_arn = lookup(var.restore_to_point_in_time, "source_db_instance_automated_backups_arn", null)
+      source_dbi_resource_id                   = lookup(var.restore_to_point_in_time, "source_dbi_resource_id", null)
+      use_latest_restorable_time               = lookup(var.restore_to_point_in_time, "use_latest_restorable_time", null)
+    }
+  }
+
   depends_on = [
     aws_db_subnet_group.default,
     aws_security_group.default,
