815f7a0e05c7270df54d9d9ac5aeb747829fb018
RoseSecurity <72598486+RoseSecurity@users.noreply.github.com>
2025-12-11 16:42:23 -0500

    chore: add descriptions for example outputs to fix bats tests (#199)
    * chore: add descriptions for example outputs to fix bats tests
    
    * chore: add input variable descriptions

--- examples/complete/outputs.tf
+++ examples/complete/outputs.tf
@@ -39,13 +39,16 @@ output "hostname" {
 }
 
 output "public_subnet_cidrs" {
-  value = module.subnets.public_subnet_cidrs
+  value       = module.subnets.public_subnet_cidrs
+  description = "Public subnet CIDRs"
 }
 
 output "private_subnet_cidrs" {
-  value = module.subnets.private_subnet_cidrs
+  value       = module.subnets.private_subnet_cidrs
+  description = "Private subnet CIDRs"
 }
 
 output "vpc_cidr" {
-  value = module.vpc.vpc_cidr_block
+  value       = module.vpc.vpc_cidr_block
+  description = "VPC CIDR blocks"
 }
